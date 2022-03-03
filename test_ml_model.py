c43403f4319db0ce4d7e384927d33dab146a9ebf
Tharindu Silva <78898203+TharinduSilva2019774@users.noreply.github.com>
2022-03-03 16:27:12 +0530

    unit test

--- test_ml_model.py
+++ test_ml_model.py
@@ -42,7 +42,7 @@ class TestAsset(TestCase):
         # 10 pounds = 4.53592 kg
         actual = float("{0:.5f}".format(Assets.poundsToKG(10)))
         expected = 4.53592
-        self.assertEqual(5555, actual)
+        self.assertEqual(expected, actual)
     #
     # def test_inches_to_cm(self):
     #     self.fail()
