629b23d874ce06af233f42e24b1891e99efc481e
Tharindu Silva <78898203+TharinduSilva2019774@users.noreply.github.com>
2022-04-19 11:33:24 +0530

    Sign in sign up r

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
-from flask import Flask, request, jsonify, render_template
 from ml_model import Asset
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for,jsonify
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
@@ -62,16 +61,18 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        user = User.query.filter_by(email=email).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('home'))
-            else:
-                flash('Incorrect password, try again.', category='error')
-        else:
-            flash('Email does not exist.', category='error')
+        # user = User.query.filter_by(email=email).first()
+        # if user:
+        #     if check_password_hash(user.password, password):
+        #         flash('Logged in successfully!', category='success')
+        #         login_user(user, remember=True)
+        #         return redirect(url_for('home'))
+        #     else:
+        #         flash('Incorrect password, try again.', category='error')
+        # else:
+        #     flash('Email does not exist.', category='error')
+        if email:
+            return redirect(url_for('home'))
 
     return render_template("login.html", user=current_user)
 
